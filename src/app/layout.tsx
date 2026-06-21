7702c2f7a48fbc549eb6b68963018dd05e1c0560
Frank A. <fasathor@gmail.com>
2026-06-21 19:00:23 +0200

    feat: add sonner toast provider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { Toaster } from '@/components/ui/sonner'
 import './globals.css'
 
 const geistSans = Geist({
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="h-full">{children}</body>
+      <body className="h-full">
+        {children}
+        <Toaster richColors closeButton position="bottom-right" />
+      </body>
     </html>
   )
 }
